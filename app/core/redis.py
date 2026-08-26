f6029b11fbd39148482dd3269542c6eaee9201e2
Eva Antonushkina <eant367@mail.ru>
2026-08-26 16:03:16 +1100

    chore(redis): add async redis client

---
+++ app/core/redis.py
@@ -0,0 +1,8 @@
+from redis.asyncio import Redis
+from app.core.config import settings
+
+redis_client: Redis = Redis.from_url(
+  settings.redis_url,
+  encoding="utf-8",
+  decode_responses=True,
+)
